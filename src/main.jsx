d82d74f1b53f737b0b86727d8739175ce1a2376d
Anuj_keshri <anujxyt@gmail.com>
2026-03-13 16:02:41 +0530

    Explore and community page

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import App from './App.jsx'
 import { Route, RouterProvider, createBrowserRouter } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import RegistrationPage from './pages/Registration'
+import ExplorePage from './pages/ExplorePage.jsx'
+import CommunityPage from './pages/CommunityPage.jsx'
+
 
 
 const router = createBrowserRouter([
@@ -12,6 +15,9 @@ const router = createBrowserRouter([
     children: [
       {path: '/', element: <HomePage/>},
       {path: '/signup', element: <RegistrationPage/>},
+      {path: '/login', element: <h1>Login Page</h1>},
+      {path: '/explore', element: <ExplorePage/>},
+      {path: '/communities', element:<CommunityPage/> },
    ],
   }
 ])
---
+++ src/pages/CommunityPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Community from '../components/community/Community'
+
+function CommunityPage() {
+  return (
+    <Community/>
+  )
+}
+
+export default CommunityPage
